acd060f8ef379552afaec368546ed6bcd6cad6ce
rhj1216 <rhj1216@gmail.com>
2023-03-11 01:36:53 +0900

    start setting

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
+import React, { Component } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Main from './pages/MainPage';
 
-import './App.css';
+const App = () => {
+	return (
+		<div className='App'>
+			<BrowserRouter>
+				{/* 헤더 */}
+				<Routes>
+					<Route path="/" element={<Main />}></Route>
+          {/* 회사소개 */}
+          {/* 렌탈 */}
+        </Routes>
+        {/* 풋터 */}
+			</BrowserRouter>
 
-function App() {
-  return (
-  <>
-  asd
-  </>
-  );
-}
+		</div>
+	);
+};
 
 export default App;
