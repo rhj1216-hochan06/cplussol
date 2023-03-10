d0e4dbee1d78f15773e8f7edb8dca8876f441332
rhj1216 <rhj1216@gmail.com>
2023-03-11 02:31:07 +0900

    headersetting

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Main from './pages/MainPage';
+import Header1 from './components/main/Header';
+
 
 const App = () => {
 	return (
 		<div className='App'>
 			<BrowserRouter>
-				{/* 헤더 */}
+      <Header1/>
 				<Routes>
 					<Route path="/" element={<Main />}></Route>
-          {/* 회사소개 */}
-          {/* 렌탈 */}
         </Routes>
-        {/* 풋터 */}
 			</BrowserRouter>
 
 		</div>
