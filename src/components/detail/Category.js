702556efdd33aa63b757def5f8968f2277c09ccc
rhj1216 <rhj1216@gmail.com>
2023-03-16 18:21:08 +0900

    디테일 페이지 카테고리 페이지 생성만

---
+++ src/components/detail/Category.js
@@ -0,0 +1,18 @@
+import * as React from "react";
+import CssBaseline from "@mui/material/CssBaseline";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import { Grid } from "@mui/material";
+
+const Category = () => {
+  return (
+    <Box>
+      <CssBaseline />
+      <Container component="main" sx={{ mt: 8, mb: 2 }} maxWidth="lg">
+        카테고리
+      </Container>
+    </Box>
+  );
+};
+export default Category;
