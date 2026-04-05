e7b8e680ab018f7714de6ded03520e0cae5e5a11
Ryu Hojin <rhj1216@gmail.com>
2026-04-05 21:26:15 +0900

    style(헤더): 로고 정렬과 연락처 디자인 개선

--- src/components/main/Header.js
+++ src/components/main/Header.js
@@ -1,43 +1,61 @@
 import FaxIcon from "@mui/icons-material/Fax";
 import PhoneIcon from "@mui/icons-material/Phone";
-import { Grid } from "@mui/material";
 import Box from "@mui/material/Box";
 import CustomAppBar from "./CustomAppBar.js";
 import styles from "./Header.module.css";
+
 export default function Header1() {
   return (
     <Box>
-      <Box sx={{}}>
+      <Box>
         <div className={styles.container1}>
-          <div className={styles.item1} >
-            <Grid item xs={1} sx={{}}>
+          <div className={styles.item1}>
+            <div className={styles.logoBox}>
               <a href="/">
-                <img src="/images/logo/leftlogo.jpg" height="100vh" />
+                <img
+                  className={styles.leftLogo}
+                  src="/images/logo/leftlogo.jpg"
+                  alt="씨플러스솔루션 심볼 로고"
+                />
               </a>
-            </Grid>
-          </div>
-          <div className={styles.item2} >
-            <Grid item xs={1} sx={{}}>
-              <a href="/">
-                <img src="/images/logo/mainlogo-removebg.png" height="80vh" />
-              </a>
-            </Grid>
-          </div>
-          <div className={styles.item3} >
-            <Grid item xs={1} sx={{ mx: 5, my: 2, minWidth: "250px" }}>
-              <a href="tel:02-2622-8081">
-                <Grid item xs={4} sx={{ mx: 5, display: "flex" }}>
-                  <PhoneIcon fontSize="large" />
-                  <Grid sx={{ my: 1 }}>전화상담: 02-2622-8081~2</Grid>
-                </Grid>
-                <Grid item xs={4} sx={{ mx: 5, display: "flex" }}>
-                  <FaxIcon fontSize="large" />
-                  <Grid sx={{ my: 1 }}>팩스번호: 02-2622-8083</Grid>
-                </Grid>
-              </a>
-            </Grid>
+            </div>
           </div>
 
+          <div className={styles.item2}>
+            <div className={styles.logoBox}>
+              <a href="/">
+                <img
+                  className={styles.mainLogo}
+                  src="/images/logo/mainlogo-removebg.png"
+                  alt="씨플러스솔루션 메인 로고"
+                />
+              </a>
+            </div>
+          </div>
+
+          <div className={styles.item3}>
+            <div className={styles.contactCard}>
+              <a className={styles.contactRow} href="tel:02-2622-8081">
+                <span className={styles.contactIcon}>
+                  <PhoneIcon fontSize="small" />
+                </span>
+                <span className={styles.contactText}>
+                  <span className={styles.contactLabel}>전화 상담</span>
+                  <span className={styles.contactValue}>02-2622-8081~2</span>
+                </span>
+              </a>
+              <div className={styles.contactDivider} />
+              <div className={styles.contactRowStatic}>
+                <span className={styles.contactIcon}>
+                  <FaxIcon fontSize="small" />
+                </span>
+                <span className={styles.contactText}>
+                  <span className={styles.contactLabel}>팩스 번호</span>
+                  <span className={styles.contactValue}>02-2622-8083</span>
+                </span>
+              </div>
+            </div>
+          </div>
         </div>
       </Box>
       <CustomAppBar />
